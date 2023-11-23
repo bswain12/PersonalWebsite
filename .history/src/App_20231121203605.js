336a5d6d028b556124aa43fad3927bb87363c5b2
bswain12 <bswain@bu.edu>
2023-11-23 17:05:38 -0500

    Trying to add pages

---
+++ .history/src/App_20231121203605.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './App.py';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello this is a new test again</h1>
+        <h2Hi a
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './App.py';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route }
+    from 'react-router-dom';
+import Home from './pages';
+import About from './pages/about';
+import Blogs from './pages/blogs';
+import SignUp from './pages/signup';
+import Contact from './pages/contact';
 
 function App() {
   return (
@@ -11,6 +19,16 @@ function App() {
         <h1>Hello this is a new test again</h1>
         <h2>I like head-ings</h2>
       </header>
+      <Router>
+            <Navbar />
+            <Routes>
+                <Route exact path='/' exact element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/contact' element={<Contact />} />
+                <Route path='/blogs' element={<Blogs />} />
+                <Route path='/sign-up' element={<SignUp />} />
+            </Routes>
+        </Router>
     </div>
   );
 }
